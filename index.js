17f6a71608a00d188b6e6ff7b3cc9f98cc4e843b
Brisa Carter <brisa.carter@gmail.com>
2019-05-15 16:11:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function updateObjectWithKeyAndValue(object, key, value){
     return Object.assign({},object,{[key]: value})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  let newObj = {
+    object: key: value
+  }
 }
 
 function deleteFromObjectByKey(object, key){
