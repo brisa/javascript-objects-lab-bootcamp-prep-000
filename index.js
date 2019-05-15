3fa191de0a0bf9e0a8c0a24e7c1d54073c242400
Brisa Carter <brisa.carter@gmail.com>
2019-05-15 16:01:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let recipes = new Object({keys: '0'})
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newObj = Object.assign({}, object, {key:value})
-  return newObj
+  
+  return Object.assign({}, object, {key:value})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   
